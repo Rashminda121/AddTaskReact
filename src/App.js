80455e70cf9168ffe8b7920625f9b08d12d3867c
rashminda <rashmindaeducation@gmail.com>
2023-10-01 08:12:38 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -5,26 +5,43 @@ import { useState } from 'react';
 
 function App() {
   
-  const [count,setCount]=useState(0);
+const [todoList, setTodolist] =useState([]);
+const [newTask,setnewTask] =useState("");
 
+const handleChange=(event)=>{
+  setnewTask(event.target.value);
+}
+
+const addTask=()=>{
+  setTodolist([...todoList,newTask]);
+}
+
+const deleteTask=(taskName)=>{
+  const newtodoList = todoList.filter ((task)=>{
+    if (task === taskName){
+      return false;
+    }else{
+      return true;
+    }
+  });
+  setTodolist(newtodoList);
+}
 
   return (
     <div className="App">
-      <button onClick={()=>{
-        setCount(count+1);
-        }}>Increase</button>
-
-      <button onClick={()=>{
-        setCount(count-1);
-      }}>Decrease</button>
-
-      <button onClick={()=>{
-        setCount(0);
-      }}>Set To Zero</button>
-
-      <br/>
-
-      {count}
+      <div className="addTask">
+          <input type="text" onChange={handleChange}/>
+          <button onClick={addTask}>Add Task</button>
+      </div>
+      <div clsssName="list">
+          {todoList.map((task)=>{
+            return (
+            <div> 
+              <h1>{task}</h1>
+              <button oncClick={() => deleteTask(task)}>x</button>
+            </div>);
+          })}
+      </div>
     
     </div>
   );
